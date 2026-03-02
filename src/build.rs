fd084059861e4b4240fdc6b32cfcf219bb50ba7d
sourcecorearch <sourcecorearch@gmail.com>
2026-03-02 13:47:04 -0300

    update build.rs cargo.toml

--- src/build.rs
+++ src/build.rs
@@ -1,5 +1,7 @@
-glib_build_tools::compile_resources(
-    &["."],
-    "core-tools.gresource.xml",
-    "core-tools.gresource",
-);
+fn main() {
+    glib_build_tools::compile_resources(
+        &["."],
+        "core-tools.gresource.xml",
+        "core-tools.gresource",
+    );
+}
